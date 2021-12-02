6ffb1387a8db5497be919e578e388ec3e3b701b1
zenjen-11 <jen.nicholls@teksavvy.com>
2021-12-02 15:12:59 +0000

    First commit.

---
+++ client.js
@@ -0,0 +1,30 @@
+const net = require('net');
+const { IP, PORT } = require("./constants");
+
+
+// Makes connection with the game server
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: IP,
+    port: PORT
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+    conn.write('Name: ' + NAME);
+
+  });
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+
+
+
+  return conn;
+};
+
+module.exports = connect;
